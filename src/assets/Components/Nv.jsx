b02e47d349df1acae8db7e4fe101fd991e6daa36
raghavendra_kashyap <rkahyap2003@gmail.com>
2025-06-08 18:00:55 +0530

    Dark mode navbar

--- src/assets/Components/Nv.jsx
+++ src/assets/Components/Nv.jsx
@@ -2,22 +2,21 @@ import React from 'react'
 export default function Nv() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary" >
-  <div className="container-fluid ">
-    <a className="navbar-brand" href="#">Dr.Vehicle</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-      
-      </ul>
-    </div>
-  </div>
-</nav>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary"data-bs-theme="dark" >
+      <div className="container-fluid ">
+        <a className="navbar-brand" href="#">Dr.Vehicle</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+          </ul>
+      </div>
+      </div>
+    </nav>
     </>
   )
 }
